8f9ec15a1b41f925a14a891476dde2f40639c086
Steve Shreeve <steve.shreeve@gmail.com>
2025-10-21 01:00:47 -0600

    Hard coded fix to v29/lib/coffeescript/parser.js -- We need to see how to get this automatically

--- v29/lib/coffeescript/parser.js
+++ v29/lib/coffeescript/parser.js
@@ -1103,139 +1103,188 @@ break;
 }
 },
   parseError(str, hash) {
-      var error
-      if (hash.recoverable) {
-        return this.trace(str)
-      } else {
-        error = new Error(str)
-        error.hash = hash
-        throw error
-      }
-    },
+    if (hash.recoverable) {
+      return this.trace(str)
+    }
+
+    const error = new Error(str)
+    error.hash = hash
+    throw error
+  },
   parse(input) {
-      var EOF, TERROR, action, errStr, expected, k, len, lex, lexer, loc, locFirst, locLast, newState, p, parseTable, preErrorSymbol, r, ranges, recovering, ref, ref1, ref2, sharedState, state, stk, symbol, v, val, yyleng, yylineno, yyloc, yytext, yyval
-      [stk, val, loc] = [[0], [null], []]
-      [parseTable, yytext, yylineno, yyleng, recovering] = [this.parseTable, '', 0, 0, 0]
-      [TERROR, EOF] = [2, 1]
-      lexer = Object.create(this.lexer)
-      sharedState = {
-        yy: {}
-      }
-      ref = this.yy
-      for (k in ref) {
-        if (!hasProp.call(ref, k)) continue
-        v = ref[k]
-        sharedState.yy[k] = v
-      }
-      lexer.setInput(input, sharedState.yy)
-      [sharedState.yy.lexer, sharedState.yy.parser] = [lexer, this]
-      if (lexer.yylloc == null) {
-        lexer.yylloc = {}
-      }
-      yyloc = lexer.yylloc
-      loc.push(yyloc)
-      ranges = (ref1 = lexer.options) != null ? ref1.ranges : void 0
-      this.parseError = typeof sharedState.yy.parseError === 'function' ? sharedState.yy.parseError : Object.getPrototypeOf(this).parseError
-      lex = () => {
-        var token
-        token = lexer.lex() || EOF
-        if (typeof token !== 'number') {
-          token = this.symbolIds[token] || token
+    // Modern destructuring and initialization
+    let [stk, val, loc] = [[0], [null], []]
+    const [TERROR, EOF] = [2, 1]
+
+    const parseTable = this.parseTable
+    let [yytext, yylineno, yyleng, recovering] = ['', 0, 0, 0]
+
+    // Create lexer with modern syntax
+    const lexer = Object.create(this.lexer)
+    const sharedState = { yy: {} }
+
+    // Use Object.entries for cleaner iteration
+    for (const [k, v] of Object.entries(this.yy)) {
+      sharedState.yy[k] = v
+    }
+
+    lexer.setInput(input, sharedState.yy)
+    Object.assign(sharedState.yy, { lexer, parser: this })
+
+    // Use nullish coalescing
+    lexer.yylloc ??= {}
+    let yyloc = lexer.yylloc
+    loc.push(yyloc)
+
+    // Optional chaining for ranges
+    const ranges = lexer.options?.ranges
+
+    // Cleaner conditional assignment
+    this.parseError = typeof sharedState.yy.parseError === 'function'
+      ? sharedState.yy.parseError
+      : Object.getPrototypeOf(this).parseError
+
+    // Arrow function for lex
+    const lex = () => {
+      const token = lexer.lex() || EOF
+      return typeof token !== 'number'
+        ? this.symbolIds[token] || token
+        : token
+    }
+
+    // Use let for mutable variables
+    let symbol = null
+    let preErrorSymbol = null
+    let state, action, r, yyval = {}
+    let p, len, newState, expected
+
+    // Main parsing loop
+    while (true) {
+      state = stk[stk.length - 1]
+
+      // Optional chaining for action lookup
+      symbol ??= lex()
+      action = this.defaultActions[state] || parseTable[state]?.[symbol]
+
+      // Handle errors with modern syntax
+      if (!action?.length || !action[0]) {
+        let errStr = ''
+
+        if (!recovering) {
+          // Use filter/map for cleaner expected tokens
+          expected = Object.entries(parseTable[state] || {})
+            .filter(([p]) => this.tokenNames[p] && p > TERROR)
+            .map(([p]) => `'${this.tokenNames[p]}'`)
         }
-        return token
+
+        // Template literals for error messages
+        errStr = lexer.showPosition
+          ? `Parse error on line ${yylineno + 1}:
+${lexer.showPosition()}
+Expecting ${expected.join(', ')}, got '${this.tokenNames[symbol] || symbol}'`
+          : `Parse error on line ${yylineno + 1}: Unexpected ${
+              symbol === EOF ? "end of input" : `'${this.tokenNames[symbol] || symbol}'`
+            }`
+
+        this.parseError(errStr, {
+          text: lexer.match,
+          token: this.tokenNames[symbol] || symbol,
+          line: lexer.yylineno,
+          loc: yyloc,
+          expected
+        })
+
+        throw new Error(errStr)
       }
-      [symbol, preErrorSymbol, state, action, r, yyval, p, len, newState, expected] = [null, null, null, null, null, {}, null, null, null, null]
-      while (true) {
-        state = stk[stk.length - 1]
-        action = this.defaultActions[state] || (symbol == null ? symbol = lex() : void 0, (ref2 = parseTable[state]) != null ? ref2[symbol] : void 0)
-        if (!((action != null ? action.length : void 0) && action[0])) {
-          errStr = ''
-          if (!recovering) {
-            expected = (function() {
-              var ref3, results
-              ref3 = parseTable[state]
-              results = []
-              for (p in ref3) {
-                if (!hasProp.call(ref3, p)) continue
-                if (this.tokenNames[p] && p > TERROR) {
-                  results.push(`'${this.tokenNames[p]}'`)
-                }
-              }
-              return results
-            }).call(this)
+
+      // Check for ambiguous actions
+      if (action[0] instanceof Array && action.length > 1) {
+        throw new Error(`Parse Error: multiple actions possible at state: ${state}, token: ${symbol}`)
+      }
+
+      // Execute action with switch
+      switch (action[0]) {
+        case 1: // Shift
+          stk.push(symbol, action[1])
+          val.push(lexer.yytext)
+          loc.push(lexer.yylloc)
+          symbol = null
+
+          if (!preErrorSymbol) {
+            // Destructuring assignment
+            ;[yyleng, yytext, yylineno, yyloc] = [
+              lexer.yyleng,
+              lexer.yytext,
+              lexer.yylineno,
+              lexer.yylloc
+            ]
+
+            if (recovering > 0) recovering--
+          } else {
+            [symbol, preErrorSymbol] = [preErrorSymbol, null]
           }
-          errStr = lexer.showPosition ? `Parse error on line ${yylineno + 1}:\n${lexer.showPosition()}\nExpecting ${expected.join(', ')}, got '${this.tokenNames[symbol] || symbol}'` : (`Parse error on line ${yylineno + 1}: Unexpected ${symbol === EOF ? "end of input" : `'${this.tokenNames[symbol] || symbol}'`}`, this.parseError(errStr, {
-            text: lexer.match,
-            token: this.tokenNames[symbol] || symbol,
-            line: lexer.yylineno,
-            loc: yyloc,
-            expected
-          }))
-          throw new Error(errStr)
+          break
+
+        case 2: { // Reduce - needs braces for const declarations
+          len = this.ruleData[action[1]][1]
+          yyval.$ = val[val.length - len]
+
+          const [locFirst, locLast] = [
+            loc[loc.length - (len || 1)],
+            loc[loc.length - 1]
+          ]
+
+          yyval._$ = {
+            first_line: locFirst.first_line,
+            last_line: locLast.last_line,
+            first_column: locFirst.first_column,
+            last_column: locLast.last_column,
+            ...(ranges && { range: [locFirst.range[0], locLast.range[1]] })
+          }
+
+          r = this.performAction.apply(yyval, [
+            yytext, yyleng, yylineno, sharedState.yy,
+            action[1], val, loc
+          ])
+
+          if (r != null) yyval.$ = r
+
+          if (len) {
+            stk.length -= len * 2
+            val.length -= len
+            loc.length -= len
+          }
+
+          stk.push(this.ruleData[action[1]][0])
+          val.push(yyval.$)
+          loc.push(yyval._$)
+
+          newState = parseTable[stk[stk.length - 2]][stk[stk.length - 1]]
+          stk.push(newState)
+          break
         }
-        if (action[0] instanceof Array && action.length > 1) {
-          throw new Error(`Parse Error: multiple actions possible at state: ${state}, token: ${symbol}`)
-        }
-        switch (action[0]) {
-          case 1: // shift
-            stk.push(symbol, action[1])
-            val.push(lexer.yytext)
-            loc.push(lexer.yylloc)
-            symbol = null
-            if (!preErrorSymbol) {
-              [yyleng, yytext, yylineno, yyloc] = [lexer.yyleng, lexer.yytext, lexer.yylineno, lexer.yylloc]
-              if (recovering > 0) {
-                recovering--
-              }
-            } else {
-              [symbol, preErrorSymbol] = [preErrorSymbol, null]
-            }
-            break
-          case 2: // reduce
-            len = this.ruleData[action[1]][1]
-            yyval.$ = val[val.length - len]
-            [locFirst, locLast] = [loc[loc.length - (len || 1)], loc[loc.length - 1]]
-            yyval._$ = {
-              first_line: locFirst.first_line,
-              last_line: locLast.last_line,
-              first_column: locFirst.first_column,
-              last_column: locLast.last_column
-            }
-            if (ranges) {
-              yyval._$.range = [locFirst.range[0], locLast.range[1]]
-            }
-            r = this.performAction.apply(yyval, [yytext, yyleng, yylineno, sharedState.yy, action[1], val, loc])
-            if (r != null) {
-              yyval.$ = r
-            }
-            if (len) {
-              stk.length -= len * 2
-              val.length -= len
-              loc.length -= len
-            }
-            stk.push(this.ruleData[action[1]][0])
-            val.push(yyval.$)
-            loc.push(yyval._$)
-            newState = parseTable[stk[stk.length - 2]][stk[stk.length - 1]]
-            stk.push(newState)
-            break
-          case 3: // accept
-            return val[val.length - 1]
-        }
+
+        case 3: // Accept
+          return val[val.length - 1]
       }
-    },
+    }
+  },
+
   trace() {},
-  yy: {},
+
+  yy: {}
 }
 
 function createParser(yyInit = {}) {
-  const p = Object.create(parserInstance);
+  const p = Object.create(parserInstance)
+
   Object.defineProperty(p, "yy", {
     value: { ...yyInit },
     enumerable: false,
     writable: true,
     configurable: true,
   })
+
   return p
 }
 
